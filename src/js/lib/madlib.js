b2c7ad8febe022ee172d0977d411acd52d2381d6
Will Franklin <will.franklin@guardian.co.uk>
2015-10-23 14:20:44 +0100

    Basic embed

---
+++ src/js/lib/madlib.js
@@ -0,0 +1,38 @@
+export default function (el, onchange) {
+    var text = el.querySelector('.madlib__input__text');
+    var btn = el.querySelector('.madlib__input__btn');
+    var currentValue = '';
+
+    function submit() {
+        currentValue = text.value;
+        onchange(text.value);
+        text.blur();
+        btn.removeAttribute('data-focus');
+    }
+
+    text.addEventListener('focus', () => { btn.setAttribute('data-focus', ''); });
+    text.addEventListener('blur', evt => {
+        // Wait for new activeElement
+        setTimeout(() => {
+            if (document.activeElement !== btn) {
+                text.value = currentValue;
+                btn.removeAttribute('data-focus');
+            }
+        }, 0);
+    });
+
+    el.addEventListener('submit', evt => {
+        evt.preventDefault();
+        submit();
+    });
+
+    btn.addEventListener('click', evt => {
+        evt.preventDefault();
+        if (btn.hasAttribute('data-focus')) {
+            submit();
+        } else {
+            currentValue = text.value = '';
+            text.focus();
+        }
+    });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,93 @@
 import iframeMessenger from 'guardian/iframe-messenger'
 import reqwest from 'reqwest'
+import doT from 'olado/doT'
+import madlib from './lib/madlib'
+import sendEvent from './lib/event'
+import geocode from './lib/geocode'
+
 import mainHTML from './text/main.html!text'
+import statsHTML from './text/stats.html!text'
+import statsRaw from './data/stats.tsv!text'
+
+var statsTemplateFn = doT.template(statsHTML);
+
+var stats = statsRaw.split('\n').map(function (stat) {
+    var [force, id, ...numbers] = stat.split('\t');
+    var [population, current, applications, appointments] = numbers.map(n => parseFloat(n));
+    return {
+        force,
+        id,
+        population,
+        current,
+        applications,
+        appointments
+    };
+}).filter(s => s.force);
+
+var overall = stats.reduce(function (a, b) {
+    return {
+        'force': 'Overall',
+        'id': 'overall',
+        'population': a.population + b.population,
+        'current': a.current + b.current,
+        'applications': a.applications + b.applications,
+        'appointments': a.appointments + (b.appointments || 0)
+    };
+});
+
+overall.population /= stats.length;
+overall.current /= stats.length;
+
+stats.push(overall);
+
+var forces = stats.map(s => s.force);
 
 var el = document.querySelector('#interactive');
+el.innerHTML = mainHTML;
+
+var statsEl = el.querySelector('.js-stats');
+var userLocationEl = el.querySelector('.js-gps');
+
+function locateForce(lat, lng) {
+    reqwest({
+        'url': `https://data.police.uk/api/locate-neighbourhood?q=${lat},${lng}`,
+        'type': 'json',
+        'crossOrigin': true,
+        'success': resp => changeForce(resp.force)
+    });
+}
+
+function changeForce(forceId) {
+    var forceStats = stats.find(s => s.id === forceId);
+    statsEl.innerHTML = statsTemplateFn({'stats': forceStats});
+}
+
+madlib(el.querySelector('.js-postcode'), loc => {
+    geocode(loc, (err, resp) => {
+        if (!err) {
+            locateForce(resp.features[0].center[1], resp.features[0].center[0]);
+        }
+    });
+});
+
+if ('geolocation' in navigator) {
+    userLocationEl.style.display = 'block';
+    userLocationEl.addEventListener('click', () => {
+        userLocationEl.removeAttribute('data-has-error');
+        userLocationEl.setAttribute('data-is-loading', '');
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            locateForce(position.coords.latitude, position.coords.longitude);
+            userLocationEl.removeAttribute('data-is-loading');
+        }, function (err) {
+            console.log(err);
+            userLocationEl.removeAttribute('data-is-loading');
+            userLocationEl.addAttribute('data-has-error', '');
+        });
+
+        userLocationEl.blur();
+    });
+}
 
 iframeMessenger.enableAutoResize();
-
-reqwest({
-    url: 'http://ip.jsontest.com/',
-    type: 'json',
-    crossOrigin: true,
-    success: (resp) => el.innerHTML = `Your IP address is ${resp.ip}`
-});
+changeForce('overall');
